d64ea2b7c1020d7d697eef10e184c64be24050ac
Sergio10G <sejobmx@hotmail.es>
2022-11-02 01:26:26 +0100

    Added special case for 2 number lists

--- src/algorithm.c
+++ src/algorithm.c
@@ -73,9 +73,11 @@ void	arrange_big(t_list **a, t_list **b)
 
 void	arrange(t_list **a, t_list **b)
 {
+	if (ft_lstsize(*a) == 2)
+		swap_a(a);
 	if (ft_lstsize(*a) == 3)
 		arrange_3(a);
-	if (ft_lstsize(*a) <= 10)
+	else if (ft_lstsize(*a) <= 10)
 		arrange_10(a, b);
 	else
 		arrange_big(a, b);
